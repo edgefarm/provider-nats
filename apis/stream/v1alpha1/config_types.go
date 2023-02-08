6b304c0e122216356b040088b9b00980a1eb402a
Armin Schlegel <armin.schlegel@gmx.de>
2023-02-08 10:51:32 +0100

    chore: updated copyright
    Signed-off-by: Armin Schlegel <armin.schlegel@gmx.de>

--- apis/stream/v1alpha1/config_types.go
+++ apis/stream/v1alpha1/config_types.go
@@ -1,7 +1,7 @@
 package v1alpha1
 
 /*
-Copyright 2023 The EdgeFarm Authors.
+Copyright 2023 The Crossplane Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
